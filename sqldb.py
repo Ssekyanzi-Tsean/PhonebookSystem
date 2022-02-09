64577e4bd810502b7f8a4672a963821f80d636f5
Shogun <msean481@gmail.com>
2022-02-09 13:56:50 +0300

    sql code that creates datbase and table

---
+++ sqldb.py
@@ -0,0 +1,22 @@
+# SQl Code that creates database Run this first
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+engine = create_engine('sqlite:///storage.db', echo=True)
+
+
+class SqlDb(Base):
+
+    __tablename__ = 'Information'
+
+    location = Column(String, primary_key=True)
+    data = Column(String)
+
+
+if __name__ == "__main__":
+    print("Creating Database")
+    Base.metadata.create_all(engine)
+    #print(f" using {__tablename__} Table ")
