c7c9a5695c1eb743a8b7fb8855a20b6b50b95b7b
Shogun <msean481@gmail.com>
2022-02-10 10:42:17 +0300

    Added main content

---
+++ main.py
@@ -0,0 +1,22 @@
+from filesystem import FileSystemDatabase
+from phonebook import PhoneBookSystem
+from inmemory import InmemoryDatabase
+from sqlsystem import SqlDatabase
+
+#database_service = FileSystemDatabase()
+#database_service = InmemoryDatabase()
+database_service = SqlDatabase()
+phonebook_system = PhoneBookSystem(database_service)
+phonebook_system.set_up_system()
+
+
+name = "Sean"
+name2 = "Ssekyanzi"
+phone = "0701901797"
+phone2 = "0788901797"
+
+
+#phonebook_system.create_contact({"name": name, "phone": phone})
+print(phonebook_system.read_contact({"name": name, "phone": phone}))
+#phonebook_system.update_contact({"name": name2, "phone": phone})
+#phonebook_system.delete_contact({"name": name2, "phone": phone})
