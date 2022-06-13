ce3e18df3a1ab177b93869295e58b1561afc4e90
Shogun <msean481@gmail.com>
2022-06-13 03:46:35 +0300

    Finally pushed data online success

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 import os
 #DATABASE_URI = 'postgresql://postgres:sekyanzi481@localhost:5432/phones'
-DATABASE_URL = "postgresql://yvnhpihjugbyxa:4f6560d3dd1f887b5bce260a63cbb5b774442d08d611e9225bc5a7bd557ef3f2@ec2-52-204-195-41.compute-1.amazonaws.com:5432/dffl6d0ihlld9t"
+#DATABASE_URL = "postgresql://yvnhpihjugbyxa:4f6560d3dd1f887b5bce260a63cbb5b774442d08d611e9225bc5a7bd557ef3f2@ec2-52-204-195-41.compute-1.amazonaws.com:5432/dffl6d0ihlld9t"
+DATABASE_URL = "postgresql://ldprlisskjgyzz:2e5679414ad4bcdd16c4328a1b5248eba3e05192af14f3a62a0dba6e12b72e3d@ec2-3-226-163-72.compute-1.amazonaws.com:5432/da4r181far79j3"
